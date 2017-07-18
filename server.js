f164a82195ca198b482b42384d1daead3be90b1d
System <none>
2017-07-18 11:41:44 +0000

    ☔️🙉 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,120 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var pubSubHubbub = require("./private/pubsubhubbub.js"); // the "official api" doesn't really do what I need
+var querystring = require("querystring");
+var bodyParser = require('body-parser');
+
+const fetch = require('node-fetch');
+const URL = require('url').URL;
+//var multer = require('multer'); // v1.0.5
+
+const callbackPath = 'https://beryl-clover.glitch.me/pubsubhubbub/';
+var pubSubSubscriber = pubSubHubbub.createServer({
+  callbackUrl: callbackPath
+});
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/subscribe", urlencodedParser, function (request, response) {
+  
+  if(request.body === undefined) {
+    response.sendStatus(400);
+    return;
+  }
+  response.sendStatus(200);
+  
+  const topic = request.body.topic;
+  const hub = request.body.hub;
+  const endpoint = request.body.endpoint;
+  
+  const privateSubscriber = pubSubHubbub.createServer({
+    callbackUrl: `${callbackPath}${querystring.escape(endpoint)}/`
+  });
+      
+  privateSubscriber.subscribe(topic, hub, function(err){
+        if(err){
+            console.log("Failed subscribing");
+        }
+    });
+});
+
+app.post("/ubsubscribe", function (request, response) {
+  response.sendStatus(200);
+});
+
+pubSubSubscriber.on("feed", data => data => {
+  console.log("feed", data);
+
+  const subscribeCallback = new URL(data.callback);
+  const endpointEncoded = subscribeCallback.pathname.replace('/pubsubhubbub/', '').replace(/\/$/,"");
+  const endpoint = querystring.unescape(endpointEncoded);
+
+  const payload =  {
+    title: `New post on ${data.topic}`,
+    body: `There is a new post at ${data.topic} via ${data.hub}`,
+    url: `${data.topic}`
+  };
+  
+  fetch(endpoint, {method:"post", headers: {"content-type": "application/json"}, "content-type": "application/json", body: payload})
+    .then(response=>console.log)
+    .catch(err=>console.error)
+});
+
+pubSubSubscriber.on("subscribe", data => {
+  console.log("subscribe", data);
+
+  const subscribeCallback = new URL(data.callback);
+  const endpointEncoded = subscribeCallback.pathname.replace('/pubsubhubbub/', '').replace(/\/$/,"");
+  const endpoint = querystring.unescape(endpointEncoded);
+
+  const payload =  {
+    title: `Subscribed to ${data.topic}`,
+    body: `Subscribed to ${data.topic} on ${data.hub}`
+  };
+  
+  console.log(endpoint)
+  
+  fetch(endpoint, {method:"post", headers: {"content-type": "application/json"}, body: JSON.stringify(payload)})
+    .then(response=>console.log)
+    .catch(err=>console.error)
+
+});
+
+pubSubSubscriber.on("unsubscribe", data => {
+  console.log("unsubscribe", data);
+
+  const subscribeCallback = new URL(data.callback);
+  const endpointEncoded = subscribeCallback.pathname.replace('/pubsubhubbub/', '').replace(/\/$/,"");
+  const endpoint = querystring.unescape(endpointEncoded);
+
+  const payload =  {
+    title: `Unsubscribed from ${data.topic}`,
+    body: `Unsubscribed from ${data.topic} on ${data.hub}`
+  };
+  
+  fetch(endpoint, {method:"post", headers: {"content-type": "application/json"}, body: payload})
+    .then(response=>console.log)
+    .catch(err=>console.error)
+});
+
+pubSubSubscriber.on("error", data => console.log("error", data));
+
+app.get("/pubsubhubbub/:endpoint/", pubSubSubscriber._onGetRequest.bind(pubSubSubscriber));
+app.post("/pubsubhubbub/:endpoint/", pubSubSubscriber._onPostRequest.bind(pubSubSubscriber));
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
